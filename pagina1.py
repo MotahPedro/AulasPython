3858ffb638efb801dd9f248ad7cc9867e0e92a62
PEDRO HENRIQUE MOTA <1090482223001@fatec-srv.local>
2024-03-05 21:40:13 -0300

    Rotas fitas mas erro no blocks

--- pagina1.py
+++ pagina1.py
@@ -4,11 +4,19 @@ app = Flask(__name__) #Instanciar objeto com flask
 
 @app.route('/') #Definição de rota (Neste caso a rota raiz.)
 def index(): # Função de nome index
-    return render_template('minhapagina.html') #Conteúdo da rota
+    return render_template('base.html') #Conteúdo da rota 1
 
-@app.route('/alunos')
-def nova_rota_alunos():
-    return "<h1>Pagina de alunos<h1>"
+@app.route('/sobre')
+def rota_sobre():
+    return render_template('sobre.html') #Conteúdo da rota 2
+
+@app.route('/servicos')
+def rota_servicos():
+    return render_template('servicos.html') #Conteúdo da rota 3
+
+@app.route('/contato')
+def rota_contato():
+    return render_template('contato.html') #Conteúdo da rota 4
 
 @app.route('/outrarota')
 def outra():
